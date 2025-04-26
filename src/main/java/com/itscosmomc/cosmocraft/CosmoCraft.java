f78caf82fe58f097ed37a89571641749f84cdbb6
ItsCosmoMC <jackcaine7@live.co.uk>
2025-04-26 19:42:31 +0100

    Very important changes

--- src/main/java/com/itscosmomc/cosmocraft/CosmoCraft.java
+++ src/main/java/com/itscosmomc/cosmocraft/CosmoCraft.java
@@ -15,7 +15,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// Very important comment
+// Very important changes
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CosmoCraft.MOD_ID)
 public class CosmoCraft {
